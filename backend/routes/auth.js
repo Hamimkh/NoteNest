fc50ffd0fa60634c8994993881759b0d8ff57f34
Hamim K <133521471+Hamimkh@users.noreply.github.com>
2023-11-28 21:53:09 +0600

    login and get user data

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,8 +4,10 @@ const User = require("../model/Users");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const fetchuser = require("../middleware/fetchusers");
 
 const JWT_SECRET = "ShamimHossain/btc"; 
+
 
 // Route-1: Creating a user using post: "/api/auth/createuser". Doesn't require authentication.
 router.post(
@@ -60,5 +62,62 @@ router.post(
       }
     }
   );
+
+
+  // Route-2: Authenticate a user using post: "/api/auth/login". No login required.
+router.post(
+  "/login",
+  [body("email").isEmail(), body("password").exists()],
+  async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      // Find the user by email
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        let success = false
+        return res.status(500).json({ success, error: "Invalid email or password" });
+      }
+
+      // Compare the provided password with the hashed password in the database
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        let success = false
+        return res.status(500).json({ success, error: "Invalid email or password" });
+      }
+
+      // Create a data object for the JWT payload
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      // Sign a JWT token
+      const authToken = jwt.sign(data, JWT_SECRET);
+      let success = true;
+      res.json({success,authToken });
+    } catch (error) {
+      console.error("Error during login:", error);
+      res.status(500).json({ error: "Server error" });
+    }
+  }
+);
+
+
+// Route-3: Get logged-in user details using post: "/api/auth/getuser". Login required.
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    // The user ID is available in req.user.id due to the fetchuser middleware
+    const userId = req.user.id;
+    // Find the user by their ID and exclude the password field from the response
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
   module.exports = router;
