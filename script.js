263d3ad7a9f09bb0fc62ba6b903ffa1bb21aa3fb
Hamim K <133521471+Hamimkh@users.noreply.github.com>
2023-12-02 22:46:38 +0600

    commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //Hamburger menu logic
+let editedNote;
+
 document.addEventListener("DOMContentLoaded", function () {
   const navbar = document.getElementById("navbar");
   const menuIcon = document.getElementById("menuIcon");
@@ -143,6 +145,8 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+let noteId;
+
 // Get user notes
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -185,9 +189,64 @@ document.addEventListener("DOMContentLoaded", function () {
       const noteCard = document.createElement("div");
       noteCard.className = "card custom-card m-2";
       noteCard.style = "width: 12rem; height: 15rem;";
+      noteCard.dataset.noteId = note._id; // Set the noteId as a data attribute
 
       const cardBody = document.createElement("div");
-      cardBody.className = "card-body custom-card-body";
+      cardBody.className = "card-body";
+
+      // Add Font Awesome icon
+      const editicon = document.createElement("i");
+      editicon.className = "fa-solid fa-pen edit-icon"; // Adjust the class based on the desired icon
+      editicon.style = "font-size: 1rem; margin: 1rem; color: green";
+      cardBody.appendChild(editicon);
+
+      // Add an event listener for the edit icon
+      editicon.addEventListener("click", function () {
+        // Set the noteId globally
+        noteId = newNote._id;
+
+        // Fetch the complete note data from your array or API based on the noteId
+        const clickedNote = notes.find((note) => note._id === noteId);
+
+        // Log values for debugging
+        console.log("Clicked noteId:", noteId);
+        console.log("Clicked note:", clickedNote);
+
+        // Check if the note is found
+        if (clickedNote) {
+          // Call the function to handle editing the note when the edit icon is clicked
+          editNoteModal.show();
+          fillEditNoteForm(clickedNote);
+        } else {
+          console.error("Note not found for ID:", noteId);
+
+          // Handle the case where the note is not found (optional)
+          // You can show an error message or handle it in a way that makes sense for your application.
+        }
+      });
+
+      cardBody.appendChild(editicon);
+
+      const deleteicon = document.createElement("i");
+      deleteicon.className = "fa-solid fa-trash"; // Adjust the class based on the desired icon
+      deleteicon.style = "font-size: 1rem; margin: 1rem; color: green";
+      cardBody.appendChild(deleteicon);
+
+      // Add an event listener for the delete icon
+      deleteicon.addEventListener("click", function () {
+        const confirmDelete = confirm(
+          "Are you sure you want to delete this note?"
+        );
+        if (confirmDelete) {
+          const noteIdToDelete = note._id; // Assuming you have access to the note ID
+          deleteNote(noteIdToDelete);
+        }
+      });
+
+      const shareicon = document.createElement("i");
+      shareicon.className = "fa-solid fa-share"; // Adjust the class based on the desired icon
+      shareicon.style = "font-size: 1rem; margin: 1rem; color: green";
+      cardBody.appendChild(shareicon);
 
       const cardTitle = document.createElement("h4");
       cardTitle.className = "card-title mb-2";
@@ -201,29 +260,56 @@ document.addEventListener("DOMContentLoaded", function () {
       cardBody.appendChild(cardText);
       noteCard.appendChild(cardBody);
 
+      // Set an id for the note card based on the note's id
+      noteCard.id = `note-${note._id}`;
+
       notesContainer.appendChild(noteCard);
+    });
+
+    // After adding new note cards, reattach event listeners
+    attachNoteEventListeners();
+  }
+
+  // Function to attach event listeners for dynamically created note cards
+  function attachNoteEventListeners() {
+    // Add an event listener for the "Save Changes" button in the edit note modal
+    const saveChangesButton = document.getElementById("saveEditNoteBtn");
+    saveChangesButton.addEventListener("click", function () {
+      // Check if noteId is defined before calling saveEditedNote
+      if (noteId !== null) {
+        saveEditedNote();
+        updateNoteCard(noteId, editedNote);
+        // You may also want to hide the modal here
+        editNoteModal.hide();
+      } else {
+        // Handle the case for new notes
+        saveNewNote();
+        // You may also want to hide the modal here
+        createNoteModal.hide();
+      }
     });
   }
 
   // Add an event listener to the "Create New Note" button
-  const createNoteButton = document.querySelector(
-    "#notes-container .btn-success"
-  );
-  if (createNoteButton) {
-    createNoteButton.addEventListener("click", function () {
-      // Show the create note modal
-      const createNoteModal = new bootstrap.Modal(
-        document.getElementById("createNoteModal")
-      );
-      createNoteModal.show();
-    });
-  }
+  document.addEventListener("DOMContentLoaded", function () {
+    // Add an event listener to the "Create New Note" button
+    const createNoteButton = document.querySelector(
+      "#notes-container .btn-success"
+    );
+    if (createNoteButton) {
+      createNoteButton.addEventListener("click", function () {
+        // Show the create note modal
+        const createNoteModal = new bootstrap.Modal(
+          document.getElementById("createNoteModal")
+        );
+        createNoteModal.show();
+      });
+    }
+  });
 
   // Call the function to fetch and display notes when the page loads
   fetchAndDisplayNotes();
 });
-
-
 
 //add note
 const createNoteModal = new bootstrap.Modal(
@@ -260,6 +346,9 @@ async function addNote() {
       if (response.ok) {
         const newNote = await response.json();
 
+        // Reset noteId for new notes
+        noteId = null;
+
         // Create a new note card
         const newNoteCard = document.createElement("div");
         newNoteCard.className = "card custom-card m-2";
@@ -270,17 +359,56 @@ async function addNote() {
 
         // Add Font Awesome icon
         const editicon = document.createElement("i");
-        editicon.className = "fa-solid fa-pen"; // Adjust the class based on the desired icon
+        editicon.className = "fa-solid fa-pen edit-icon";
         editicon.style = "font-size: 1rem; margin: 1rem; color: green";
         cardBody.appendChild(editicon);
 
+        editicon.addEventListener("click", function () {
+          noteId = newNoteCard.dataset.noteId;
+          const noteTitle = newNoteCard.dataset.noteTitle;
+          const noteDescription = newNoteCard.dataset.noteDescription;
+
+          // Call the function to handle editing the note when the edit icon is clicked
+          editNoteModal.show();
+          fillEditNoteForm({
+            _id: noteId,
+            title: noteTitle,
+            description: noteDescription,
+          });
+
+          // Add an event listener for the "Save Changes" button in the edit note modal
+          const saveChangesButton = document.getElementById("saveEditNoteBtn");
+          saveChangesButton.addEventListener("click", function () {
+            // Check if noteId is defined before calling saveEditedNote
+            if (noteId !== null) {
+              saveEditedNote();
+              updateNoteCard(noteId, editedNote);
+              // You may also want to hide the modal here
+              editNoteModal.hide();
+            } else {
+              console.error("NoteId is undefined. Cannot save edited note.");
+            }
+          });
+        });
+
         const deleteicon = document.createElement("i");
-        deleteicon.className = "fa-solid fa-trash"; // Adjust the class based on the desired icon
+        deleteicon.className = "fa-solid fa-trash";
         deleteicon.style = "font-size: 1rem; margin: 1rem; color: green";
         cardBody.appendChild(deleteicon);
 
+        // Add an event listener for the delete icon
+        deleteicon.addEventListener("click", function () {
+          const confirmDelete = confirm(
+            "Are you sure you want to delete this note?"
+          );
+          if (confirmDelete) {
+            const noteIdToDelete = note._id; // Assuming you have access to the note ID
+            deleteNote(noteIdToDelete);
+          }
+        });
+
         const shareicon = document.createElement("i");
-        shareicon.className = "fa-solid fa-share"; // Adjust the class based on the desired icon
+        shareicon.className = "fa-solid fa-share";
         shareicon.style = "font-size: 1rem; margin: 1rem; color: green";
         cardBody.appendChild(shareicon);
 
@@ -296,7 +424,6 @@ async function addNote() {
         cardBody.appendChild(cardText);
         newNoteCard.appendChild(cardBody);
 
-        // Add the new note card to the notes container
         notesContainer.appendChild(newNoteCard);
 
         // Clear input fields
@@ -311,5 +438,125 @@ async function addNote() {
     } catch (error) {
       console.error("Error adding note:", error);
     }
+  }
+}
+
+// Edit note
+
+// edit note modal
+const editNoteModal = new bootstrap.Modal(
+  document.getElementById("editNoteModal")
+);
+
+function fillEditNoteForm(note) {
+  const editNoteTitleInput = document.getElementById("editNoteTitle");
+  const editNoteContentInput = document.getElementById("editNoteContent");
+
+  // Check if the note object is valid
+  if (
+    note &&
+    note._id !== undefined &&
+    note.title !== undefined &&
+    note.description !== undefined
+  ) {
+    // Fill the form with existing note data
+    editNoteTitleInput.value = note.title;
+    editNoteContentInput.value = note.description;
+  } else {
+    console.error("Invalid note object in fillEditNoteForm");
+  }
+}
+
+async function saveEditedNote() {
+  const editedNoteTitle = document.getElementById("editNoteTitle").value;
+  const editedNoteContent = document.getElementById("editNoteContent").value;
+
+  const editedNoteData = {
+    title: editedNoteTitle,
+    description: editedNoteContent,
+  };
+
+  try {
+    // Ensure noteId is defined
+    if (noteId) {
+      const response = await fetch(
+        `http://localhost:5000/api/notes/updatenote/${noteId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token": localStorage.getItem("authToken"),
+          },
+          body: JSON.stringify(editedNoteData),
+        }
+      );
+
+      if (response.ok) {
+        // Update the global variable editedNote with the edited note data
+        editedNote = await response.json();
+
+        // Handle success, maybe update the note card with the edited details
+        updateNoteCard(noteId, editedNote);
+
+        // Hide the edit note modal
+        editNoteModal.hide();
+      } else {
+        console.error("Failed to edit note:", response.statusText);
+      }
+    } else {
+      console.error("NoteId is undefined. Cannot save edited note.");
+    }
+  } catch (error) {
+    console.error("Error editing note:", error);
+  }
+}
+
+// Function to update the note card with edited details
+function updateNoteCard(noteId, editedNote) {
+  const noteCard = document.getElementById(`note-${noteId}`);
+  if (noteCard) {
+    // Update the card details with the edited note details
+    const cardTitle = noteCard.querySelector(".card-title");
+    const cardText = noteCard.querySelector(".card-text");
+
+    if (editedNote && cardTitle && cardText) {
+      // Check if title and description properties are defined in editedNote
+      if (
+        editedNote.note &&
+        editedNote.note.title !== undefined &&
+        editedNote.note.description !== undefined
+      ) {
+        cardTitle.textContent = editedNote.note.title;
+        cardText.textContent = editedNote.note.description;
+      }
+    }
+  }
+}
+
+// Delete note
+
+async function deleteNote(noteId) {
+  try {
+    const response = await fetch(`http://localhost:5000/api/notes/deletenote/${noteId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('authToken'),
+      },
+    });
+
+    if (response.ok) {
+      // Note deleted successfully, remove the corresponding card from the UI
+      const noteCardToDelete = document.getElementById(`note-${noteId}`);
+      if (noteCardToDelete) {
+        noteCardToDelete.remove();
+      }
+    } else if (response.status === 404) {
+      console.error('Note not found:', response.statusText);
+    } else {
+      console.error('Failed to delete note:', response.statusText);
+    }
+  } catch (error) {
+    console.error('Error deleting note:', error);
   }
 }
