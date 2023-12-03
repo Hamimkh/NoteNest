033da4718df3d27837839b74cb54d89bdd05616f
Hamim K <133521471+Hamimkh@users.noreply.github.com>
2023-12-03 12:11:42 +0600

    commit

--- notenest/src/components/Login.js
+++ notenest/src/components/Login.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import logo from './images/logo.png';
 import './Authenticate.css';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 function Login() {
+  const [credintials, setCredintials] = useState({email:"", password:""});
+  let navigate = useNavigate()
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    const response = await fetch(
+      `http://localhost:5000/api/auth/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email: credintials.email, password: credintials.password}),
+      }
+    );
+    const json = await response.json();
+    console.log(json)
+    if(json.success){
+      // Save the auth token and redirect
+      localStorage.setItem('token', json.authtoken);
+      navigate('/userui');
+
+    }
+  }
+  const handleChange = (e) => {
+    setCredintials({ ...credintials, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <section>
@@ -14,9 +41,9 @@ function Login() {
           <h1 className="authenticate-head">Log In</h1>
           <hr className="line" />
 
-          <form className="authenticate-form" action="#" method="POST" id="loginForm">
-            <input type="email" id="email" name="email" placeholder="Email" required />
-            <input type="password" id="password" name="password" placeholder="Password" required />
+          <form className="authenticate-form" onSubmit={handleSubmit}>
+            <input type="email" id="email" name="email"  onChange={handleChange} placeholder="Email" required />
+            <input type="password" id="password" name="password"  onChange={handleChange} placeholder="Password" required />
             <button type="submit" className="authenticate-btn">
               Login
             </button>
