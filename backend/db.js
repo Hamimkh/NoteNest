230c7dd1465635ded5a44f64667947428e007102
Hamim K <133521471+Hamimkh@users.noreply.github.com>
2023-12-29 22:52:46 +0600

    updated

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://127.0.0.1:27017/NoteNest';
+require('dotenv').config();
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(mongoURI);
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log('Connected to MongoDB successfully!');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
--- notenest/src/components/Noteitems.js
+++ notenest/src/components/Noteitems.js
@@ -16,13 +16,21 @@ function Noteitems(props) {
       }
     };
 
+    const handleDeleteNote = () => {
+      const shouldDelete = window.confirm("Are you sure you want to delete this note?");
+      if (shouldDelete) {
+        deleteNote(note._id);
+        props.showAlert("Note Deleted Successfully!", "success");
+      }
+    };
+
   return (
     <>
       <div id="notes-container">
           <div  className="card custom-card m-2" style={{ width: '12rem', height: '15rem'}}>
             <div className='card-icon'>
           <i className="fa-solid fa-edit mx-2" onClick={()=>{updateNote(note)}} style={{fontSize: "1rem", marginLeft: "1rem", color: "green", width:"2rem", cursor:"pointer"}} ></i>
-          <i className="fa-solid fa-trash mx-2" onClick={()=>{deleteNote(note._id);  props.showAlert("Note Deleted Successfully!", "success");}} style={{fontSize: "1rem", marginLeft: "1rem", color: "red", width:"2rem", cursor:"pointer"}}></i>
+          <i className="fa-solid fa-trash mx-2" onClick={handleDeleteNote} style={{fontSize: "1rem", marginLeft: "1rem", color: "red", width:"2rem", cursor:"pointer"}}></i>
           <i className="fa-solid fa-share mx-2"  onClick={handleShareNote} style={{fontSize: "1rem", marginLeft: "1rem", color: "blue", width:"2rem", cursor:"pointer"}}></i>
           </div>
             <h4 className="card-title mb-4 mx-2">{note.title}</h4>
